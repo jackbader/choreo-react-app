22cc1f00c88473b9d366a7406035351a8ccbabb7
Jack Bader <jack@culpritunderwear.com>
2024-04-28 21:10:43 -0400

    adding authentication

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import Cookies from 'js-cookie';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <button
+          onClick={() => {
+            window.location.href = '/auth/login';
+          }}>
+          Login
+        </button>
+        <button
+          onClick={async () => {
+            window.location.href = `/auth/logout?session_hint=${Cookies.get('session_hint')}`;
+          }}>
+          Logout
+        </button>
+        <button
+          onClick={async () => {
+            const response = await fetch('/auth/userinfo');
+            console.log('response', response);
+          }}>
+          Test Auth
+        </button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -13,8 +33,7 @@ function App() {
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
       </header>
